496efdde1fc4dcae2207b56d907abf3b06f017ea
Rajeeb321123 <starsgazer321@gmail.com>
2023-02-05 22:10:26 +0545

    ...

--- next.config.js
+++ next.config.js
@@ -3,13 +3,14 @@
 
 
 // if you get Error: Invalid src prop (https://lh3.googleusercontent.com/a/AEdFTp6xKvLTKjuujE_d3C-cGFqg7UOj6Uvaq2d_HqTZ=s96-c) on `next/image`, hostname "lh3.googleusercontent.com" is not configured under images in your `next.config.js`
-
 const nextConfig = {
   // for ignoring the dev and build errors in deployment of app in vercel.com , we can write ignore code like below
   typescript:{
     ignoreBuildErrors:true,
   },
 
+
+  
   reactStrictMode: true,
   
   // configuring images hostname
